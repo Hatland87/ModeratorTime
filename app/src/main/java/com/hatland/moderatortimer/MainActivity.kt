c9b052f36368b7accf1d49adb5f95775e0379a0d
Hatland <hatland.oyvind@gmail.com>
2020-04-30 05:27:27 +0200

    added textViews and buttons with simple functionality

--- app/src/main/java/com/hatland/moderatortimer/MainActivity.kt
+++ app/src/main/java/com/hatland/moderatortimer/MainActivity.kt
@@ -3,28 +3,58 @@ package com.hatland.moderatortimer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tidInnlegg = 0
+    private var tidReplikk = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buttonPress()
     }
 
+    private fun buttonPress() {
+        textViewTimeInnlegg.text = tidInnlegg.toString()
+        textViewTimeReplikk.text = tidReplikk.toString()
+    }
+
+    fun merTidInnlegg (view: View) {
+        tidInnlegg++
+        buttonPress()
+    }
+
+    fun mindreTidInnlegg (view: View) {
+        tidInnlegg--
+        buttonPress()
+    }
+
+    fun merTidReplikk (view: View) {
+        tidReplikk++
+        buttonPress()
+    }
+
+    fun mindreTidReplikk (view: View) {
+        tidReplikk--
+        buttonPress()
+    }
+
+    // fullscreen
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                )
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
     }
 }
